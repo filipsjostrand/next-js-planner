19411e2248adc3b85734f9f3d4d2368b4b56fa95
filipsjostrand <filip_sjostrand@hotmail.com>
2026-03-04 17:58:18 +0100

    FIX auth.config.ts (allowed strings)

--- auth.config.ts
+++ auth.config.ts
@@ -44,7 +44,10 @@ export const authConfig = {
       // Överför data från JWT till Session så att vi kommer åt det i client components (useSession)
       if (session.user && token) {
         session.user.id = token.id as string
-        session.user.role = token.role as string
+
+        // FIX: Här castar vi till de specifika tillåtna rollerna istället för 'string'
+        session.user.role = token.role as "ADMIN" | "USER" | "GUEST"
+
         session.user.groupId = token.groupId as string
       }
       return session
@@ -54,50 +57,3 @@ export const authConfig = {
     signIn: "/login", // Vår custom login-sida
   },
 } satisfies NextAuthConfig
-
-// import type { NextAuthConfig } from "next-auth"
-
-// export const authConfig = {
-//   providers: [], // Vi lämnar denna tom här, den fylls i av auth.ts (Node-miljön)
-//   callbacks: {
-//     authorized({ auth, request: { nextUrl } }) {
-//       const isLoggedIn = !!auth?.user
-//       const isOnDashboard = nextUrl.pathname === "/"
-//       const isAuthRoute = nextUrl.pathname.startsWith("/login")
-
-//       // Om man är på startsidan och inte är inloggad -> skicka till login
-//       if (isOnDashboard) {
-//         if (isLoggedIn) return true
-//         return false
-//       }
-
-//       // Om man är inloggad och försöker gå till login-sidan -> skicka till start
-//       if (isAuthRoute && isLoggedIn) {
-//         return Response.redirect(new URL("/", nextUrl))
-//       }
-
-//       return true
-//     },
-//     async jwt({ token, user }) {
-//       // 'user' kommer från authorize-funktionen i auth.ts första gången man loggar in
-//       if (user) {
-//         token.id = user.id as string
-//         token.role = user.role
-//         token.groupId = user.groupId
-//       }
-//       return token
-//     },
-//     async session({ session, token }) {
-//       // Här mappar vi från vår utökade JWT-typ till Session-typen
-//       if (session.user && token) {
-//         session.user.id = token.id
-//         session.user.role = token.role
-//         session.user.groupId = token.groupId
-//       }
-//       return session
-//     },
-//   },
-//   pages: {
-//     signIn: "/login",
-//   },
-// } satisfies NextAuthConfig
