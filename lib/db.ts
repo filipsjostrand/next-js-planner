85bc9d85c6d1a3301b707a67b6c4d80308d59998
filipsjostrand <filip_sjostrand@hotmail.com>
2026-03-04 18:11:58 +0100

    FIX db.ts and package.json (prisma connection)

--- lib/db.ts
+++ lib/db.ts
@@ -1,22 +1,17 @@
 import { Pool } from "pg"
 import { PrismaPg } from "@prisma/adapter-pg"
-import { PrismaClient } from "@prisma/client" // Standardplatsen
+import { PrismaClient } from "@prisma/client"
 
 const connectionString = process.env.DATABASE_URL
 
-if (!connectionString) {
-  throw new Error("DATABASE_URL is not defined in .env")
+declare global {
+  var prisma: PrismaClient | undefined
 }
 
 const pool = new Pool({ connectionString })
 const adapter = new PrismaPg(pool)
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
-}
+// Vi skapar klienten med adaptern för att köra via pg-pool (bra för serverless)
+export const db = global.prisma || new PrismaClient({ adapter })
 
-export const db = globalForPrisma.prisma || new PrismaClient({
-  adapter
-})
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db
+if (process.env.NODE_ENV !== "production") global.prisma = db
